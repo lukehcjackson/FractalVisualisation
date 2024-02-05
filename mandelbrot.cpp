0226f99ae62a2530ed70984601224bf22816e125
luke-hc-jackson <luke.hc.jackson@gmail.com>
2024-02-05 21:34:02 +0000

    Fixed mandelbrot set logic!
    Wasn't actually applying the function recursively!! Now have a weird result, scaling factor is wrong, but mandelbrot set is being calculated correctly!

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -7,18 +7,13 @@ sf::Uint8* mandelbrot(int WIDTH, int HEIGHT, sf::Uint8* pixels) {
     Return the modified pixel array
     */
    
-   //TODO: NEED TO SCALE THE SIZE OF THE CANVAS APPROPRIATELY!!!
    //the canvas coordinates are from 0-1600, 0-900 but we want to be looking at a scale more like -2 to 2, -2 to 2
-   float scaleFactor = 800;
-
-
+   //float scaleFactor = 800;
    int outStart = -2;
    int outEnd = 2;
-
    double xSlope = 1.0 * (outEnd - outStart) / WIDTH;
    double ySlope = 1.0 * (outEnd - outStart) / HEIGHT;
 
-   //try arduino suggestsion on stackoverflow
 
    int MAX_ITERATIONS = 50;
 
@@ -26,16 +21,17 @@ sf::Uint8* mandelbrot(int WIDTH, int HEIGHT, sf::Uint8* pixels) {
     //each i is a pixel on the screen
 
     //need an x,y coordinate
-    int x = i % WIDTH;
-    int y = i / WIDTH;
+    double x = i % WIDTH;
+    double y = i / WIDTH;
 
     //want to 'map' x,y from 0->width, 0->height to -2->2, -2->2
-    
     double newX = -2 + xSlope * x;
     double newY = -2 + ySlope * y;
     x = newX;
     y = newY;
     
+    double cx = x;
+    double cy = y;
 
     int n = 0;
     
@@ -46,15 +42,15 @@ sf::Uint8* mandelbrot(int WIDTH, int HEIGHT, sf::Uint8* pixels) {
         //so we can represent the real and imaginary parts of the result without needing to use any complex number libraries
 
         //Z^2
-        float re = x * x - y * y;
-        float im = 2 * x * y;
+        double re = x * x - y * y;
+        double im = 2 * x * y;
         //+C
-        re += x;
-        im += y;
+        x = re + cx;
+        y = im + cy;
 
         //check if the magnitude of Z is greater than 2
         //=> sqrt(re^2 + im^2) > 2 so re^2 + im^2 > 4
-        if (re * re + im * im > 4) {
+        if (x * x + y * y > 4) {
             //this point is not in the mandelbrot set
             break;
         }
@@ -70,17 +66,11 @@ sf::Uint8* mandelbrot(int WIDTH, int HEIGHT, sf::Uint8* pixels) {
     } else {
         color = 0;
     }
-
-    pixels[i] = color;
-    pixels[i + 1] = color;
-    pixels[i + 2] = color;
-    pixels[i + 3] = 255;
     
-    
-    //pixels[i] = n * 255 / MAX_ITERATIONS; //red
-    //pixels[i + 1] = n * 255 / MAX_ITERATIONS; //green
-    //pixels[i + 2] = n * 255 / MAX_ITERATIONS; //blue
-    //pixels[i + 3] = 255; //alpha
+    pixels[i] = n * 255 / MAX_ITERATIONS; //red
+    pixels[i + 1] = n * 255 / MAX_ITERATIONS; //green
+    pixels[i + 2] = n * 255 / MAX_ITERATIONS; //blue
+    pixels[i + 3] = 255; //alpha
     
    }
 
