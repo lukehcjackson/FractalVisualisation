f553354b7f4ba59564b1c24e1f3681ba70b95ab5
luke-hc-jackson <luke.hc.jackson@gmail.com>
2024-02-05 17:20:55 +0000

    Compiler and SFML setup

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
+
+/*
+
+TO COMPILE:
+
+g++ -c main.cpp -I"C:\\msys64\\ucrt64\\include\\SFML"
+g++ main.o -o main -L"C:\\msys64\\ucrt64\\lib" -lsfml-graphics -lsfml-window -lsfml-system
+./main
+
+*/
