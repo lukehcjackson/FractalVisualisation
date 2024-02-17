91103591b35bd2d2ec8e4c8b6ceb062cd17851ab
luke-hc-jackson <luke.hc.jackson@gmail.com>
2024-02-17 23:09:58 +0000

    remember h file

--- helper.h
+++ helper.h
@@ -1,2 +1,2 @@
-double plot_map(double outStart, double outEnd, double inStart, double inEnd, double point);
-double map(double outStart, double outEnd, double inStart, double inEnd, double point);
+long double plot_map(long double outStart, long double outEnd, long double inStart, long double inEnd, long double point);
+long double map(long double outStart, long double outEnd, long double inStart, long double inEnd, long double point);
